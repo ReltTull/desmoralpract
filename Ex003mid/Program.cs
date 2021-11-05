d0cec27bd20e66be46dee906a2eb019cb6d929dc
ReltTull <92668493+ReltTull@users.noreply.github.com>
2021-11-05 21:50:23 +0300

    Ex003compl

---
+++ Ex003mid/Program.cs
@@ -0,0 +1,26 @@
+﻿int [] array = new int [10];
+int index = 0;
+while (index < 10)
+{
+    array[index] = new Random().Next(10,100);
+    index++;
+}
+
+index = 0;
+
+while (index < 10)
+{
+    Console.Write(array[index] + " ");
+    index++;
+}
+
+Console.WriteLine();
+index = 0;
+while (index < 10)
+{
+    if (array[index] % 2 == 0)
+    {
+        Console.Write(array[index] + " ");
+    }
+    index++;
+}
